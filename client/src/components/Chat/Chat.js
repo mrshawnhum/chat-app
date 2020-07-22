13afec4b3bf3dec0924322c7fd55a81113aa9679
Shawn Humphreys <shawnhum97@gmail.com>
2020-07-22 06:29:06 -0500

    Finish chat functionality

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import queryString from 'query-string';
+import io from "socket.io-client";
+
+
+import {InfoBar} from '../InfoBar/InfoBar';
+import {Input} from '../Input/Input';
+import {Messages} from '../Messages/Messages';
+import {TextContainer} from '../TextContainer/TextContainer'
+
+import './Chat.css';
+
+let socket;
+
+export const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = 'localhost:5000';
+
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setRoom(room);
+    setName(name)
+
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
+    });
+  }, [ENDPOINT, location.search]);
+  
+  useEffect(() => {
+    socket.on('message', message => {
+      setMessages(msgs => [ ...msgs, message ]);
+    });
+    
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+}, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if(message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  }
+
+
+  return (
+    <div className="outerContainer">
+    <TextContainer users={users} />
+      <div className="container">
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
+    </div>
+  );
+}
