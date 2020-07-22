4d6b30f937b21e2fb45de15f79ab625682b48468
Shawn Humphreys <shawnhum97@gmail.com>
2020-07-22 06:23:52 -0500

    Add online status

---
+++ client/src/components/InfoBar/InfoBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+import closeIcon from '../../icons/closeIcon.png';
+
+import './InfoBar.css';
+
+export const InfoBar = ({ room }) => (
+  <div className="infoBar">
+    <div className="leftInnerContainer">
+      <img className="onlineIcon" src={onlineIcon} alt="online icon" />
+      <h3>{room}</h3>
+    </div>
+    <div className="rightInnerContainer">
+      <a href="/"><img src={closeIcon} alt="close icon" /></a>
+    </div>
+  </div>
+);
